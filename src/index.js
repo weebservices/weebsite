e7b770bdd9cb2d4f619b2f60043190b1c9f9eb9a
Bowser65 <contact.bowser65@gmail.com>
2019-12-04 22:10:28 +0100

    no robots

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const Providers = require('./providers')
 const providers = new Providers()
 const app = express()
 
-app.get('/robots.txt', (req, res) => res.type('text/plain').send('User-agent: *\nDisallow: /'))
+app.get('**/robots.txt', (req, res) => res.type('text/plain').send('User-agent: *\nDisallow: /'))
 
 app.get('/maid', (req, res) => providers.provide(req, res, 'MAID'))
 app.get('/maid/kinky', (req, res) => providers.provide(req, res, 'MAID_NSFW'))
